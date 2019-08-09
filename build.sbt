c32d2828c9da4b78103a0ddac47e82a6cf5f447d
Torsten Schmits <torsten.schmits@springernature.com>
2019-08-09 15:34:06 +0200

    runner

--- build.sbt
+++ build.sbt
@@ -2,8 +2,9 @@ val core = (pro(project, "core"))
   .settings(
     libraryDependencies ++= List(
       "org.typelevel" %% "cats-effect" % "2.0.0-RC1",
-      "org.scala-sbt" %% "test-interface" % "1.0"
-    )
+      "org.scala-sbt" % "test-interface" % "1.0"
+    ),
+    testFrameworks += new TestFramework("klk.KlkFramework"),
   )
 
 val root = (pro(project, "."))
--- core/src/main/scala/klk/Run.scala
+++ core/src/main/scala/klk/Run.scala
@@ -1,6 +1,45 @@
 package klk
 
-import sbt.testing.Framework
+import sbt.testing.{Framework, Fingerprint, SubclassFingerprint, Runner, Task, TaskDef}
+
+object KlkFingerprint
+extends SubclassFingerprint
+{
+  def superclassName: String =
+    "klk.Test"
+
+  def isModule: Boolean =
+    true
+
+  def requireNoArgConstructor: Boolean =
+    false
+}
+
+class KlkFramework
+extends Framework
+{
+  def name: String =
+    "kallikrein"
+
+  def fingerprints: Array[Fingerprint] =
+    Array(KlkFingerprint)
+
+  def runner(args: Array[String], remoteArgs: Array[String], testClassLoader: ClassLoader): Runner =
+    new Runner {
+      println(1)
+      def args: Array[String] =
+        Array()
+
+      def done: String =
+        ""
+
+      def remoteArgs: Array[String] =
+        Array()
+
+      def tasks(x: Array[TaskDef]): Array[Task] =
+        Array()
+    }
+}
 
 object Run
 {
